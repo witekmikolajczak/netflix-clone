d9edf63f3aa6e44d8268e35a6877ddf9c9c220ff
witekmikolajczak <witekmikolajczak@gmail.com>
2022-04-20 21:55:30 +0200

    Fix modal

--- src/pages/HomePage/MovieDetails/MovieDetails.jsx
+++ src/pages/HomePage/MovieDetails/MovieDetails.jsx
@@ -3,18 +3,27 @@ import { createPortal } from "react-dom";
 
 import classes from "./MovieDetails.module.scss";
 
+const IMAGE_URL = `https://image.tmdb.org/t/p/w300`;
+
 const Backdrop = (props) => {
   console.log(props.onClick);
   return <div className={classes.backdrop} onClick={props.onClick}></div>;
 };
 
 const MovieModal = ({ movieData, ...props }) => {
-  console.log(movieData);
+    const sectionStyle = {
+        width:'100%',
+        height:'100%',
+        backgroundImage: `url(${IMAGE_URL}${movieData.image})`
+    }
   return (
-    <div className={classes.container} onClick={props.onClick}>
-      {movieData.map((data) => (
-        <p>{data.title}</p>
-      ))}
+    <div className={classes.container}  onClick={props.onClick}>
+        <div className={classes.wrapper}>
+            <img src={`${IMAGE_URL}${movieData.image}`}/>
+            <h1>{movieData.title}</h1>
+            <p>{movieData.description}</p>
+        </div>
+
     </div>
   );
 };
