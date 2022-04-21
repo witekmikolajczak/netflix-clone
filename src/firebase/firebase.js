dd32caf363c626c03895065840556483eb31b01b
witekmikolajczak <witekmikolajczak@gmail.com>
2022-04-21 20:18:38 +0200

    Add storage bucket and storage database url

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,18 @@
 import firebase from "firebase/compat/app";
-import { getStorage } from "firebase/storage";
+import 'firebase/compat/storage'
 import "firebase/compat/auth";
 
 export const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey:"AIzaSyClPo4vll-zFIWu1QmTMZwhBa8WkMHq14E",
+  authDomain: "netflix-clone-e6dad.firebaseapp.com",
+  projectId: "netflix-clone-e6dad",
+  messagingSenderId: "728858555050",
+  appId: "1:728858555050:web:ae302c21c589f47a9fd6d3",
+  measurementId: "G-47EN6V3SSZ",
+  databaseURL: "gs://netflix-clone-e6dad.appspot.com",
+  storageBucket: "netflix-clone-e6dad.appspot.com",
 });
 export const auth = app.auth();
-export const storage = getStorage(app);
+export const storage = firebase.storage()
+
+
